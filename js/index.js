165c06d04dd1524e2b7109fb7fd7ce5e4726cde0
SC1314520 <blc0000421@gmail.com>
2023-02-19 15:09:53 +0800

    second

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 var mail;
-var referer=window.location.href;
 window.onload=function(){
-  mail =prompt("請輸入電子信箱：")
+  //mail =prompt("請輸入電子信箱：")
 }
 function previewFile() {
+    var state=document.getElementById("state");
+    state.innerHTML='<img src="" height="200" alt="Image preview" />';
     var preview = document.querySelector('img');
     var file = document.querySelector('input[type=file]').files[0];
 
@@ -19,18 +20,18 @@ function previewFile() {
   
 }
 function upload(){
-  alert("開始執行上傳...")
-  var result=document.getElementById("result");
+  var state=document.getElementById("state");
   var res=document.getElementById("res");
   var btn=document.getElementById("btn");
   var fileName = document.querySelector('input[type=file]').files[0].name;
   var base64 = document.querySelector('img').src;
+  state.innerHTML="資料上傳中..."
   var url='https://script.google.com/macros/s/AKfycbxjPht_lkb_CN7_hikjdQjZh0ru0hfX8xRINC7V8ZwTHvcWBYnHYlvXapHfpXAq76hCtg/exec'
   var formData =new FormData();
   formData.append('base64Str',base64);
   formData.append('fileName',fileName);
   formData.append('mail',mail);
-  formData.append('referer',referer);
+  formData.append('referer',location.origin);
   var config={
     method:'POST',
     body:formData,
@@ -41,19 +42,18 @@ function upload(){
     return res.text();
   })
   .then(function(data){
-    if(!data.includes("'failed upload'")){
+    console.log(data)
+    if(!data.includes("failed upload")){
       if(data.split("/")[5]!=undefined){   
         res.innerText="https://drive.google.com/uc?id="+data.split("/")[5]
       }
-      result.style.display='block';
-      btn.style.display='none';
-      alert("上傳成功");
+      state.innerHTML="<div class='result'><div class='msg'><img src='images/icons8-ok-500.png'></div><br/><div class='retext'>上傳成功，請查收郵件</div>";
     }
     else{
-      alert("上傳失敗，請重新上傳")
+      state.innerHTML="<div class='result'><div class='msg'><img src='images/icons8-cancel-500.png'></div><br/><div class='retext'>上傳失敗，請重新上傳</div>";
     }
   })
   .catch(e=>{
-    alert("上傳失敗，請重新上傳");
+    state.innerHTML="<div class='result'><div class='msg'><img src='images/icons8-cancel-500.png'></div><br/><div class='error'>上傳失敗，請重新上傳</div>";
   })
 }
